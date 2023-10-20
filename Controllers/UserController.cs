90f6b95c06a126c4adce33505a055264789019ed
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-10-21 00:27:20 +0530

    add: Dapper & MVC pattern

---
+++ Controllers/UserController.cs
@@ -0,0 +1,110 @@
+// Providing separate namespaces and using them to optimize and not load everything
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+    [HttpGet("Getusers")]
+    public IEnumerable<User> GetUsers()
+    {
+        string sql = @"
+        SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] 
+        FROM DotNetCourseDatabase.TutorialAppSchema.Users";
+
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+    }
+
+    [HttpGet("Getuser/{userId}")]
+    public User GetUser(int userId)
+    {
+        string sql = @"
+        SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] 
+        FROM DotNetCourseDatabase.TutorialAppSchema.Users
+        WHERE UserId=" + userId.ToString();
+
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
+    }
+
+    [HttpPut("EditUser")]
+    // basically not returning any specific data, just telling if req was success or failure
+    public IActionResult EditUser(User user)
+    {
+        string sql = @"
+        UPDATE TutorialAppSchema.Users
+            SET [FirstName] = '" + user.FirstName + @"',
+            [LastName] = '" + user.LastName + @"',
+            [Email] = '" + user.Email + @"',
+            [Gender] = '" + user.Gender + @"',
+            [Active] = '" + user.Active + @"' 
+        WHERE UserId = '" + user.UserId + @"'";
+
+        bool res = _dapper.ExecuteSql(sql);
+
+        if(res) return Ok(); // from controller base
+        throw new Exception("Updation failed");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(UserToAddDto user)
+    {
+        string sql = @"
+        INSERT INTO TutorialAppSchema.Users(
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active]
+        ) VALUES (
+            '" + user.FirstName + @"',
+            '" + user.LastName + @"',
+            '" + user.Email + @"',
+            '" + user.Gender + @"',
+            '" + user.Active + @"'
+        )";
+
+        bool res = _dapper.ExecuteSql(sql);
+
+        if(res) return Ok(); 
+        throw new Exception("Insertion failed");
+    }
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId){
+        string sql = @"DELETE FROM TutorialAppSchema.Users WHERE UserId = " + userId.ToString();
+
+        bool res = _dapper.ExecuteSql(sql);
+
+        if(res) return Ok(); 
+        throw new Exception("Insertion failed");
+    }
+}
